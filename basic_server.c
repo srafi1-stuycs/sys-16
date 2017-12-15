a7da74212693e24c1deeba8f06e3433639602102
Penn Wu <penn.z.wu@gmail.com>
2017-12-15 11:00:31 -0500

    done

--- basic_server.c
+++ basic_server.c
@@ -11,13 +11,13 @@ int main() {
     while (1) {
         char receive[256];
         read(from_client, receive, 256);
-        printf("Received \"%s\"\\n", receive);
+        printf("Received \"%s\"\n", receive);
 
         char send[256];
         strcpy(send, receive);
         strcat(send, "_MODIFIED");
         write(to_client, send, 256);
-        printf("Sent \"%s\"\\n", send);
+        printf("Sent \"%s\"\n", send);
     }
     // remove upstream pipe
     close(to_client);
